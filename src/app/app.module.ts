c7b13d66e95ea06704c82ca106ca53e3c0c51452
Chase Baker <chasebaker@Chases-MBP.attlocal.net>
2020-04-19 13:50:34 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DisplayComponent } from './components/display/display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DisplayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
